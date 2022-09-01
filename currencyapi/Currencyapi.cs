bbdb16c30af60c270b5fc602b6a29bbc5a52e7f7
Benedikt Stoy <benedikt.stoy@everapi.com>
2022-09-01 11:56:38 +0200

    Version 1.0.0

---
+++ currencyapi/Currencyapi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlTypes;
+using ipbase.Helpers;
+
+namespace currencyapi
+{
+    public class Currencyapi
+    {
+        private string ApiKey { get; }
+
+        public Currencyapi()
+        {
+        }
+
+        public Currencyapi(string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+
+        public string Status()
+        {
+            return RequestHelper.Status(ApiKey);
+        }
+
+        public string Currencies(string currencies = "")
+        {
+            return RequestHelper.Currencies(ApiKey, currencies);
+        }
+
+        public string Latest(string baseCurrency = "USD", string currencies = "")
+        {
+            return RequestHelper.Latest(ApiKey, baseCurrency, currencies);
+        }
+
+        public string Historical(string data, string baseCurrency = "USD", string currencies = "")
+        {
+            return RequestHelper.Historical(ApiKey, data, baseCurrency, currencies);
+        }
+
+        public string Range(string datetimeStart, string datetimeEnd, string accuracy = "day", string baseCurrency = "USD", string currencies = "")
+        {
+            return RequestHelper.Range(ApiKey, datetimeStart, datetimeEnd, accuracy, baseCurrency, currencies);
+        }
+
+        public string Convert(string value, string date, string baseCurrency = "USD", string currencies = "")
+        {
+            return RequestHelper.Convert(ApiKey, value, date, baseCurrency, currencies);
+        }
+    }
+}
+
---
+++ currencyapi/Helpers/RequestHelper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ipbase.Helpers
+{
+    public class RequestHelper
+    {
+        public const string BaseUrl = "https://api.currencyapi.com/v3/";
+
+        public RequestHelper()
+        {
+        }
+
+        public static string Status(string apiKey = null)
+        {
+            string url;
+            url = BaseUrl + "/status?apikey=" + apiKey;
+
+            return GetResponse(url);
+        }
+
+        public static string Currencies(string apiKey, string currencies)
+        {
+            string url;
+            url = BaseUrl + "/info?currencies=" + currencies + "&apikey=" + apiKey;
+
+            return GetResponse(url);
+        }
+
+        public static string Latest(string apiKey, string baseCurrency, string currencies)
+        {
+            string url;
+            url = BaseUrl + "/info?currencies=" + currencies + "&base_currency=" + baseCurrency + "&apikey=" + apiKey;
+
+            return GetResponse(url);
+        }
+
+        public static string Historical(string apiKey, string date, string baseCurrency, string currencies)
+        {
+            string url;
+            url = BaseUrl + "/info?currencies=" + currencies + "&base_currency=" + baseCurrency + "&date=" + date + "&apikey=" + apiKey;
+
+            return GetResponse(url);
+        }
+
+        public static string Range(string apiKey, string datetimeStart, string datetimeEnd, string accuracy, string baseCurrency, string currencies)
+        {
+            string url;
+            url = BaseUrl + "/info?currencies=" + currencies + "&base_currency=" + baseCurrency + "&datetime_start=" + datetimeStart + "&datetime_end=" + datetimeEnd + "&accuracy=" + accuracy + "&apikey=" + apiKey;
+
+            return GetResponse(url);
+        }
+
+        public static string Convert(string apiKey, string value, string date, string baseCurrency, string currencies)
+        {
+            string url;
+            url = BaseUrl + "/info?currencies=" + currencies + "&base_currency=" + baseCurrency + "&date=" + date + "&value=" + value + "&apikey=" + apiKey;
+
+            return GetResponse(url);
+        }
+
+
+        private static string GetResponse(string url)
+        {
+            string jsonString;
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            try
+            {
+                using (var response = (HttpWebResponse)request.GetResponse())
+                using (var stream = response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    jsonString = reader.ReadToEnd();
+                }
+            }
+            catch (WebException e)
+            {
+                using (WebResponse response = e.Response)
+                {
+                    HttpWebResponse httpResponse = (HttpWebResponse)response;
+                    Console.WriteLine("Error code: {0}", httpResponse.StatusCode);
+                    using (Stream data = response.GetResponseStream())
+                    using (var reader = new StreamReader(data))
+                    {
+                        jsonString = reader.ReadToEnd();
+                    }
+                }
+            }
+
+
+            return jsonString;
+        }
+    }
+}
+
